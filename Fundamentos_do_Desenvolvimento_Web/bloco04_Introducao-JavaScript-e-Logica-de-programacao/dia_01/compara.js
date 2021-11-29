4e7fcef1628ea7f07994d4778d8992b165b7e3e9
lucasbaq <lucasbaquintela@gmail.com>
2021-11-29 08:34:23 -0300

    faz um programa que compara 3  num e diz o maior

--- Fundamentos_do_Desenvolvimento_Web/bloco04_Introducao-JavaScript-e-Logica-de-programacao/dia_01/compara.js
+++ Fundamentos_do_Desenvolvimento_Web/bloco04_Introducao-JavaScript-e-Logica-de-programacao/dia_01/compara.js
@@ -4,8 +4,8 @@ let b = 5;
 console.log("O maior numero foi");
 
 if (a > b){
-    console.log(a)
+    console.log(a);
 }
 if (b > a){
-    console.log(a)
+    console.log(a);
 }
---
+++ Fundamentos_do_Desenvolvimento_Web/bloco04_Introducao-JavaScript-e-Logica-de-programacao/dia_01/compara3.js
@@ -0,0 +1,25 @@
+let a= 10;
+let b = 24;
+let c = 24;
+
+if (a > b){
+    console.log(a);
+}
+else if (b > c){
+    console.log(a);
+}
+else if (c == b){
+    console.log("c: " + c);
+    console.log("b: " + b);
+}
+else if (a == b){
+    console.log("a: " + a);
+    console.log("b: " + b);
+}
+else if (a == c){
+    console.log("a: " + a);
+    console.log("c: " + c);
+}
+else{
+    console.log(c);
+}
