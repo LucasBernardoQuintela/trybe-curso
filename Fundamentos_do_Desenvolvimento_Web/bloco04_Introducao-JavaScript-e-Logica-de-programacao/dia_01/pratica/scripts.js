cefa6b021f47fb522ee7b01b5dfda94643ceec5a
lucasbaq <lucasbaquintela@gmail.com>
2021-11-29 08:07:05 -0300

    altera o valor de birthYear e imprime

--- Fundamentos_do_Desenvolvimento_Web/bloco04_Introducao-JavaScript-e-Logica-de-programacao/dia_01/pratica/scripts.js
+++ Fundamentos_do_Desenvolvimento_Web/bloco04_Introducao-JavaScript-e-Logica-de-programacao/dia_01/pratica/scripts.js
@@ -1,6 +1,11 @@
 let myName = "Lucas";
 let birthCity = "Belo Horizonte";
 let birthYear = 2002;
+
 console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
