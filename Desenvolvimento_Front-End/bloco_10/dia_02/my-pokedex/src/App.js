2cec05e12978123a78e055bdece824c9039a88a2
lucasbaq <lucasbaquintela@gmail.com>
2022-02-22 16:17:43 -0300

    atualiza

---
+++ Desenvolvimento_Front-End/bloco_10/dia_02/my-pokedex/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Pokedex from './Pokedex';
+
+function App() {
+  return (
+    <div className="pokedex-container">
+      <h1>Pokedéx</h1>
+      <Pokedex />
+    </div>
+  );
+}
+
+export default App;
